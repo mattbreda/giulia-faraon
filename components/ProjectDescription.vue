b30aec65e2f432d682b622db217d5e0f75af5f55
mattbreda <mattbreda@gmail.com>
2023-09-06 17:00:11 +0200

    Update ProjectDescription.vue

--- components/ProjectDescription.vue
+++ components/ProjectDescription.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="w-full bg-white text-black rounded-lg p-4">
-    <PrismicRichText class="font-PPEditorial text-[2rem] leading-8	mb-4" :field="content.title"/>
-    <PrismicRichText class="font-TWKLausanneULight text-title-h2 mb-8" :field="content.sub_title"/>
-    <PrismicRichText class="font-TWKLausanneULight text-title-h3" :field="content.long_text"/>
+    <PrismicRichText class="font-PPEditorial text-[2rem] leading-8	mb-4" v-if="content?.title" :field="content.title"/>
+    <PrismicRichText class="font-TWKLausanneULight text-title-h2 mb-8" v-if="content?.sub_title" :field="content.sub_title"/>
+    <PrismicRichText class="font-TWKLausanneULight text-title-h3" v-if="content?.long_text" :field="content.long_text"/>
   </div>
 </template>
 <script setup>

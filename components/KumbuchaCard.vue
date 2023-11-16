5aa0324229ab5e1349a8da570293fa0a910ddd35
mattbreda <mattbreda@gmail.com>
2023-11-16 18:53:40 +0100

    Update KumbuchaCard.vue

--- components/KumbuchaCard.vue
+++ components/KumbuchaCard.vue
@@ -3,10 +3,7 @@
     <div class="w-full flex flex-col shadow-simple rounded-lg p-4 mt-6 bg-white">
       <nuxt-img src="/images/kombu.webp" class="object-cover w-full rounded-lg" alt="Banner about kombucha" format="webp"/>
       <div class="text-titolo-h2 text-purple font-TWKLausanneULight mb-2 mt-6">Side project</div>
-      <div class="richtext text-titolo-h2 text-black font-TWKLausanneLight">Funky Fermenteria</div>
       <div class="flex flex-col grow justify-between">
-        <div class="text-titolo-h2  text-black font-TWKLausanneULight mb-6">Aiuto i brand a posizionarsi nel mercato,
-          individuando i canali di comunicazione</div>
         <div class="text-paragraph font-TWKLausanneLight link-card hover:underline text-black">Approfondisci +</div>
       </div>
     </div>
